1523175b5a7ebbb4a7c6b16ec37688e7a8f17716
Steve Delor <sdelor@us.ibm.com>
2020-03-06 00:57:28 -0500

    feat (google interview) - fetch web api
    We repleace the http module by fetch, to run requests against endpoints

---
+++ googleInterview.js
@@ -0,0 +1,37 @@
+/**
+ * Given a list of endpoints
+ * Load each one until one is successful
+ */
+
+// ENDPOINTS
+const apiUrls = [
+    "http://dummy.restapiexample.com/api/v1/employees1",
+    "http://dummy.restapiexample.com/api/v1/employees2",
+    "http://dummy.restapiexample.com/api/v1/employees3",
+    "http://dummy.restapiexample.com/api/v1/employees"
+];
+
+let fetch = require("node-fetch");
+
+let endpoint = 0;
+const myData;
+let getData = () => {
+    fetch(apiUrls[endpoint])
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            processData(data);
+        })
+        .catch(err => {
+            console.error("error")
+            if (++endpoint < apiUrls.length)
+                getData();
+        });
+};
+
+getData();
+
+function processData(data){
+    document.getElementById("")
+}
+
